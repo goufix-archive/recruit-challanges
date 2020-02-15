fe23a9eabe30e83228fabce1534f99b9fab42dd8
Goufix <alifer.17.11@gmail.com>
2020-02-15 12:39:10 -0300

    add console api

--- codenation/cesar/index.js
+++ codenation/cesar/index.js
@@ -1,16 +1,18 @@
 const { decode, encrypt } = require("./cesar");
 const fs = require("fs");
 
-function getJson() {
+function getJSON() {
   return JSON.parse(fs.readFileSync("./answer.json", "utf-8"));
 }
-const { numero_casas, cifrado: data } = getJson();
+
+const json = getJSON();
+const { numero_casas, cifrado: data } = json
 
 const decoded = decode(data, numero_casas);
 const crypted = encrypt(decoded);
 
 const newJson = JSON.stringify({
-  ...getJson(),
+  ...json,
   decifrado: decoded,
   resumo_criptografico: crypted
 });
---
+++ codenation/cesar/node.js
@@ -0,0 +1,9 @@
+const { decode } = require('./cesar');
+
+const [, , num, ...frases] = process.argv;
+
+const newFrases = frases.map((original) => ({
+  original, codificado: decode(original, parseInt(num, 10))
+}))
+
+console.log(newFrases);
